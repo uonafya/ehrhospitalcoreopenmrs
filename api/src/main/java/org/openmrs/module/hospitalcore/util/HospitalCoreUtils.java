de3c6140101dc8e155778f14d5bd4fba39a5faea
Truong Ha <truonghatsts@gmail.com>
2011-10-05 15:52:06 +0530

    added hospitalcore utils

---
+++ api/src/main/java/org/openmrs/module/hospitalcore/util/HospitalCoreUtils.java
@@ -0,0 +1,22 @@
+package org.openmrs.module.hospitalcore.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HospitalCoreUtils {
+
+	public static Map<String, Object> buildParameters(Object... objs){
+		Map<String, Object> parameters = new HashMap<String, Object>();
+		String name = null;
+		Object data = null;
+		for(int i=0; i<objs.length; i++){
+			if(i%2==0){
+				name = objs[i].toString();
+			} else {
+				data = objs[i];
+				parameters.put(name, data);
+			}
+		}
+		return parameters;
+	}
+}
