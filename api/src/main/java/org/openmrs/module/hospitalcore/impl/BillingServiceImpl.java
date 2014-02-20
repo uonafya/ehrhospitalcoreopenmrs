292bc2f654e879a715c20ef1ae526398dfebdf30
ghanshyamcusat2006 <ghanshyam.cusat2006@gmail.com>
2014-02-20 01:20:09 -0800

    redmine issue #3969 Queue generated from Billing/OPD does not show in Laboratory or Radiology

--- api/src/main/java/org/openmrs/module/hospitalcore/impl/BillingServiceImpl.java
+++ api/src/main/java/org/openmrs/module/hospitalcore/impl/BillingServiceImpl.java
@@ -897,12 +897,16 @@ public class BillingServiceImpl extends BaseOpenmrsService implements BillingSer
 					for (ConceptSet con : conceptSets) {
 						if (labConceptIds.contains(con.getConcept().getConceptId())) {
 							labEncounter = getEncounter(bill, labEncounter, labEncounterType);
+							labEncounter=Context.getEncounterService().saveEncounter(labEncounter);
 							Order order = addOrder(labEncounter, con.getConcept(), bill, labOrderType);
+							order=Context.getOrderService().saveOrder(order);
 							item.setOrder(order);
 							
 						} else if (radiologyConceptIds.contains(con.getConcept().getConceptId())) {
 							radiologyEncounter = getEncounter(bill, radiologyEncounter, radiologyEncounterType);
+							radiologyEncounter = getEncounter(bill, radiologyEncounter, radiologyEncounterType);
 							Order order = addOrder(radiologyEncounter, con.getConcept(), bill, radiologyOrderType);
+							order=Context.getOrderService().saveOrder(order);
 							item.setOrder(order);
 						}
 					}
@@ -910,23 +914,29 @@ public class BillingServiceImpl extends BaseOpenmrsService implements BillingSer
 			} else {
 				if (labConceptIds.contains(concept.getConceptId())) {
 					labEncounter = getEncounter(bill, labEncounter, labEncounterType);
+					labEncounter=Context.getEncounterService().saveEncounter(labEncounter);
 					Order order = addOrder(labEncounter, concept, bill, labOrderType);
+					order=Context.getOrderService().saveOrder(order);
 					item.setOrder(order);
 					
 				} else if (radiologyConceptIds.contains(concept.getConceptId())) {
 					radiologyEncounter = getEncounter(bill, radiologyEncounter, radiologyEncounterType);
+					radiologyEncounter = getEncounter(bill, radiologyEncounter, radiologyEncounterType);
 					Order order = addOrder(radiologyEncounter, concept, bill, radiologyOrderType);
+					order=Context.getOrderService().saveOrder(order);
 					item.setOrder(order);
 				}
 			}
 		}
 		
+		/*
 		if (labEncounter != null) {
 			Context.getEncounterService().saveEncounter(labEncounter);
 		}
 		if (radiologyEncounter != null) {
 			Context.getEncounterService().saveEncounter(radiologyEncounter);
 		}
+		*/
 		savePatientServiceBill(bill);
 	}
 	
