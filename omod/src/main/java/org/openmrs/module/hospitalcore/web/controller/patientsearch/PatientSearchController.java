13847a02e79d96a3830b976b550954dfc4b17598
ningosi <nicotwendelee@gmail.com>
2020-06-30 11:55:43 +0300

    Add respective web layer classes

--- omod/src/main/java/org/openmrs/module/hospitalcore/web/controller/patientsearch/PatientSearchController.java
+++ omod/src/main/java/org/openmrs/module/hospitalcore/web/controller/patientsearch/PatientSearchController.java
@@ -1,109 +1,120 @@
-/**
- *  Copyright 2010 Society for Health Information Systems Programmes, India (HISP India)
- *
- *  This file is part of Hospital-core module.
- *
- *  Hospital-core module is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
-
- *  Hospital-core module is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with Hospital-core module.  If not, see <http://www.gnu.org/licenses/>.
- *
- **/
-
 package org.openmrs.module.hospitalcore.web.controller.patientsearch;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.math.BigInteger;
+import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.Enumeration;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import org.apache.commons.lang.StringUtils;
 import org.openmrs.Patient;
-import org.openmrs.PersonAttribute;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.hospitalcore.HospitalCoreService;
+import org.openmrs.module.hospitalcore.matcher.AgeMatcher;
+import org.openmrs.module.hospitalcore.matcher.DateMatcher;
+import org.openmrs.module.hospitalcore.matcher.GenderMatcher;
+import org.openmrs.module.hospitalcore.matcher.Matcher;
+import org.openmrs.module.hospitalcore.matcher.RelativeNameMatcher;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@Controller("HospitalcoreSearchPatientController")
+@Controller("PatientSearchController")
+@RequestMapping({"/module/hospitalcore/patientSearch.form"})
 public class PatientSearchController {
-	
-	@RequestMapping(value = "/module/hospitalcore/searchPatient.form", method = RequestMethod.GET)
-	public String showForm(@RequestParam("searchBoxView") String searchBoxView, HttpServletRequest request, Model model)
-	                                                                                                                    throws IOException {
-		model.addAttribute("searchBoxView", searchBoxView + ".jsp");
-		return "/module/hospitalcore/patientSearchPlugin/searchPatient";
-	}
-	
-	@SuppressWarnings("rawtypes")
-	@RequestMapping(value = "/module/hospitalcore/searchPatient.form", method = RequestMethod.POST)
-	public String searchPatient(@RequestParam("query") String query, @RequestParam("view") String view,
-	                            HttpServletRequest request, Model model) {
-		List<Patient> patients = Context.getService(HospitalCoreService.class).searchPatient(query);
-		
-		for (Enumeration e = request.getParameterNames(); e.hasMoreElements();) {
-			String parameterName = (String) e.nextElement();
+	@RequestMapping(method = {RequestMethod.POST})
+	public String searchPatient(@RequestParam(value = "phrase", required = false) String phrase, @RequestParam(value = "view", required = false) String view, @RequestParam(value = "currentPage", required = false) Integer currentPage, @RequestParam(value = "pageSize", required = false) Integer pageSize, HttpServletRequest request, Model model) {
+		String prefix = Context.getAdministrationService().getGlobalProperty("hospitalcore.identifier_prefix");
+		model.addAttribute("prefix", prefix);
+		if (phrase.contains("-") && !phrase.contains(prefix))
+			phrase = prefix + phrase;
+		String gender = request.getParameter("gender");
+		if (gender.equalsIgnoreCase("any"))
+			gender = null;
+		Integer age = getInt(request.getParameter("age"));
+		Integer ageRange = getInt(request.getParameter("ageRange"));
+		String relativeName = request.getParameter("relativeName");
+		String date = request.getParameter("date");
+		Integer dateRange = getInt(request.getParameter("dateRange"));
+		HospitalCoreService hcs = (HospitalCoreService)Context.getService(HospitalCoreService.class);
+		List<Patient> patients = hcs.searchPatient(phrase, gender, age.intValue(), ageRange.intValue(), date, dateRange.intValue(), relativeName);
+		for (Enumeration<String> e = request.getParameterNames(); e.hasMoreElements(); ) {
+			String parameterName = e.nextElement();
 			model.addAttribute(parameterName, request.getParameter(parameterName));
 		}
-		Map<Integer, Map<Integer, String>> attributeMap = buildAttributeMap(patients);
-		Map<Integer, java.util.Date> lastVisitTime = getLastVisitTime(patients);
-		
-		model.addAttribute("lastVisitTime", lastVisitTime);
-		model.addAttribute("patients", patients);
-		model.addAttribute("attributeMap", attributeMap);
-		return view;
+		if (currentPage.intValue() > 0)
+			model.addAttribute("prevPage", Integer.valueOf(currentPage.intValue() - 1));
+		if ((currentPage.intValue() + 1) * pageSize.intValue() <= patients.size())
+			model.addAttribute("nextPage", Integer.valueOf(currentPage.intValue() + 1));
+		List<Patient> renderedPatients = pagePatient(patients, currentPage.intValue(), pageSize.intValue());
+		model.addAttribute("patients", renderedPatients);
+		model.addAttribute("size", Integer.valueOf(patients.size()));
+		return "/module/hospitalcore/patientSearch/" + view;
 	}
-	
-	private Map<Integer, Map<Integer, String>> buildAttributeMap(List<Patient> patients) {
-		HospitalCoreService hcs = Context.getService(HospitalCoreService.class);
-		Map<Integer, Map<Integer, String>> attributeMap = new HashMap<Integer, Map<Integer, String>>();
-		for (Patient patient : patients) {
-			Map<Integer, String> attributes = new HashMap<Integer, String>();
-			for (PersonAttribute pa : hcs.getPersonAttributes(patient.getPatientId())) {
-				attributes.put(pa.getAttributeType().getId(), pa.getValue());
+
+	private List<Patient> filterPatients(HttpServletRequest request, List<Patient> patients) throws NumberFormatException, ParseException {
+		List<Patient> filteredPatients = patients;
+		String genderCriterion = request.getParameter("gender");
+		if (!StringUtils.isBlank(genderCriterion))
+			filteredPatients = select(filteredPatients, (Matcher)new GenderMatcher(new String(genderCriterion)));
+		String ageCriterion = request.getParameter("age");
+		if (!StringUtils.isBlank(ageCriterion)) {
+			String ageRange = request.getParameter("ageRange");
+			try {
+				filteredPatients = select(filteredPatients, (Matcher)new AgeMatcher(new Integer(ageCriterion), new Integer(ageRange)));
+			} catch (Exception e) {
+				e.printStackTrace();
+				throw new NumberFormatException("advancesearch.error.age");
 			}
-			attributeMap.put(patient.getPatientId(), attributes);
 		}
-		return attributeMap;
+		String relativeNameCriterion = request.getParameter("relativeName");
+		if (!StringUtils.isBlank(relativeNameCriterion))
+			filteredPatients = select(filteredPatients, (Matcher)new RelativeNameMatcher(relativeNameCriterion));
+		String dateCriterion = request.getParameter("date");
+		if (!StringUtils.isBlank(dateCriterion))
+			try {
+				String dateRange = request.getParameter("dateRange");
+				filteredPatients = select(filteredPatients, (Matcher)new DateMatcher(dateCriterion, new Integer(dateRange)));
+			} catch (Exception e) {
+				e.printStackTrace();
+				throw new NumberFormatException("advancesearch.error.date");
+			}
+		return filteredPatients;
 	}
-	
-	/**
-	 * Get the last Visit Time of Patient - Thai Chuong: Jun 1st 2012
-	 * 
-	 * @param patients
-	 * @return Map <patient.id, LastVisitTime>
-	 */
-	private Map<Integer, java.util.Date> getLastVisitTime(List<Patient> patients) {
-		Map<Integer, java.util.Date> lastVisitTime = new HashMap<Integer, java.util.Date>();
-		HospitalCoreService hcs = Context.getService(HospitalCoreService.class);
-		for (Patient p : patients) {
-			// TO DO
-			lastVisitTime.put(p.getPatientId(), hcs.getLastVisitTime(p.getPatientId()));
+
+	private List<Patient> pagePatient(List<Patient> patients, int currentPage, int pageSize) {
+		int firstIndex = pageSize * currentPage;
+		List<Patient> page = new ArrayList<Patient>();
+		for (int i = firstIndex; i < (currentPage + 1) * pageSize; i++) {
+			if (i < patients.size())
+				page.add(patients.get(i));
 		}
-		return lastVisitTime;
+		return page;
 	}
-	
-	@RequestMapping(value = "/module/hospitalcore/getPatientResultCount.form", method = RequestMethod.POST)
-	public void getPatientResultCount(@RequestParam("query") String query, HttpServletResponse response) throws IOException {
-		response.setContentType("text/html;charset=UTF-8");
-		PrintWriter out = response.getWriter();
-		BigInteger count = Context.getService(HospitalCoreService.class).getPatientSearchResultCount(query);
-		out.print(count);
+
+	@RequestMapping(method = {RequestMethod.GET})
+	public String showSearchBox(@RequestParam("view") String view, @RequestParam("resultBoxId") String resultBoxId, Model model) {
+		model.addAttribute("view", view);
+		model.addAttribute("resultBoxId", resultBoxId);
+		return "/module/hospitalcore/patientSearch/searchBox";
+	}
+
+	private List<Patient> select(List<Patient> patients, Matcher matcher) {
+		List<Patient> result = new ArrayList<Patient>();
+		for (Patient patient : patients) {
+			if (matcher.matches(patient))
+				result.add(patient);
+		}
+		return result;
+	}
+
+	private Integer getInt(String value) {
+		try {
+			Integer number = Integer.valueOf(Integer.parseInt(value));
+			return number;
+		} catch (Exception e) {
+			return Integer.valueOf(0);
+		}
 	}
 }
