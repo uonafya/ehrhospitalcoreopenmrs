1ed1530e072c2dbe749f79d656e0fe9ddf7db5d1
ningosi <nicotwendelee@gmail.com>
2020-07-24 11:10:09 +0300

    Adjust the API layer files to fit the OpenMRS latest license

--- api/src/main/java/org/openmrs/module/hospitalcore/model/IpdPatientAdmission.java
+++ api/src/main/java/org/openmrs/module/hospitalcore/model/IpdPatientAdmission.java
@@ -20,6 +20,19 @@
 
 
 
+/*
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
 package org.openmrs.module.hospitalcore.model;
 
 import java.io.Serializable;
@@ -34,9 +47,7 @@ import org.openmrs.module.hospitalcore.util.PatientUtils;
 /**
  * 
  * <p> Class: IpdPatientAdmission </p>
- * <p> Package: org.openmrs.module.hospitalcore.model </p> 
- * <p> Author: Nguyen manh chuyen </p>
- * <p> Update by: Nguyen manh chuyen </p>
+ * <p> Package: org.openmrs.module.hospitalcore.model </p>
  * <p> Version: $1.0 </p>
  * <p> Create date: Mar 17, 2011 12:22:35 PM </p>
  * <p> Update date: Mar 17, 2011 12:22:35 PM </p>
