eca60fa217cfb0fe05fdcef22c6c3e477c153c16
ningosi <nicotwendelee@gmail.com>
2020-07-24 11:10:09 +0300

    Adjust the API layer files to fit the OpenMRS latest license

--- api/src/main/java/org/openmrs/module/hospitalcore/impl/PatientQueueServiceImpl.java
+++ api/src/main/java/org/openmrs/module/hospitalcore/impl/PatientQueueServiceImpl.java
@@ -18,7 +18,19 @@
  *
  **/
 
-
+/*
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
 
 package org.openmrs.module.hospitalcore.impl;
 
@@ -46,9 +58,7 @@ import org.openmrs.module.hospitalcore.model.TriagePatientQueueLog;
 
 /**
  * <p> Class: PatientQueueServiceImpl </p>
- * <p> Package: org.openmrs.module.hospitalcore.impl </p> 
- * <p> Author: Nguyen manh chuyen </p>
- * <p> Update by: Nguyen manh chuyen </p>
+ * <p> Package: org.openmrs.module.hospitalcore.impl </p>
  * <p> Version: $1.0 </p>
  * <p> Create date: Feb 16, 2011 12:36:59 PM </p>
  * <p> Update date: Feb 16, 2011 12:36:59 PM </p>
