864ae44f45c4958b70dc0f2765a114e76bcb1497
ningosi <nicotwendelee@gmail.com>
2021-07-28 16:55:17 +0300

    Adding missing table for the investigations at the radiology department

--- api/src/main/java/org/openmrs/module/hospitalcore/impl/PatientDashboardServiceImpl.java
+++ api/src/main/java/org/openmrs/module/hospitalcore/impl/PatientDashboardServiceImpl.java
@@ -56,11 +56,20 @@ public class PatientDashboardServiceImpl implements PatientDashboardService {
 	public void setDao(PatientDashboardDAO dao) {
 		this.dao = dao;
 	}
-	
+
 	public List<Concept> searchSymptom(String text) throws APIException {
 
-		if(dao.searchConceptsByNameFromAlistOfClasses(text) != null && dao.searchConceptsByNameFromAlistOfClasses(text).size() > 0) {
-			return dao.searchConceptsByNameFromAlistOfClasses(text);
+		ConceptClass cc =  Context.getConceptService().getConceptClassByName(PatientDashboardConstants.CONCEPT_CLASS_NAME_SYMPTOM);
+		ConceptClass ccf =  Context.getConceptService().getConceptClassByName(PatientDashboardConstants.CONCEPT_CLASS_NAME_SYMPTOM_FINDINGS);
+		ConceptClass ccd =  Context.getConceptService().getConceptClassByName(PatientDashboardConstants.CONCEPT_CLASS_NAME_DIAGNOSIS);
+		if(dao.searchConceptsByNameAndClass(text, cc) != null && dao.searchConceptsByNameAndClass(text, cc).size() > 0) {
+			return dao.searchConceptsByNameAndClass(text, cc);
+		}
+		else if(dao.searchConceptsByNameAndClass(text, ccf) != null && dao.searchConceptsByNameAndClass(text, ccf).size() > 0) {
+			return dao.searchConceptsByNameAndClass(text, ccf);
+		}
+		else if(dao.searchConceptsByNameAndClass(text, ccd) != null && dao.searchConceptsByNameAndClass(text, ccd).size() > 0) {
+			return dao.searchConceptsByNameAndClass(text, ccd);
 		}
 		return null;
 
