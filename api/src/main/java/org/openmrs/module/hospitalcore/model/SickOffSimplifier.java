c30ba3261b9da5ea9c49cdfd50dc75544b3fcaee
ningosi <nicotwendelee@gmail.com>
2023-05-25 08:53:58 +0300

    Add a sickoff simplifier class

---
+++ api/src/main/java/org/openmrs/module/hospitalcore/model/SickOffSimplifier.java
@@ -0,0 +1,65 @@
+package org.openmrs.module.hospitalcore.model;
+
+import java.io.Serializable;
+
+public class SickOffSimplifier implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String patientIdentifier;
+    private Integer sickOffId;
+    private String provider;
+
+    public String getPatientIdentifier() {
+        return patientIdentifier;
+    }
+
+    public void setPatientIdentifier(String patientIdentifier) {
+        this.patientIdentifier = patientIdentifier;
+    }
+
+    public Integer getSickOffId() {
+        return sickOffId;
+    }
+
+    public void setSickOffId(Integer sickOffId) {
+        this.sickOffId = sickOffId;
+    }
+
+    public String getProvider() {
+        return provider;
+    }
+
+    public void setProvider(String provider) {
+        this.provider = provider;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public String getPatientName() {
+        return patientName;
+    }
+
+    public void setPatientName(String patientName) {
+        this.patientName = patientName;
+    }
+
+    public SickOffSimplifier(){}
+    public SickOffSimplifier(String patientIdentifier, Integer sickOffId, String provider, String notes, String patientName) {
+        this.patientIdentifier = patientIdentifier;
+        this.sickOffId = sickOffId;
+        this.provider = provider;
+        this.notes = notes;
+        this.patientName = patientName;
+    }
+
+    private String notes;
+
+    private String patientName;
+}
