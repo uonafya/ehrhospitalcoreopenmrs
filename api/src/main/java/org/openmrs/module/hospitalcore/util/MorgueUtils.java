9b405aebe4bf99fc5a7e5c06bec348978e100e20
ningosi <nicotwendelee@gmail.com>
2023-09-26 10:44:25 +0300

    Add cleared admission status

--- api/src/main/java/org/openmrs/module/hospitalcore/util/MorgueUtils.java
+++ api/src/main/java/org/openmrs/module/hospitalcore/util/MorgueUtils.java
@@ -2,6 +2,7 @@ package org.openmrs.module.hospitalcore.util;
 
 public class MorgueUtils {
     public static final Integer PENDING_ADMISSION = 0;
+    public static final Integer CLEARED_FOR_ADMISSION = 1;
     public static final Integer ADMITTED = 2;
     public static final Integer DISCHARGED = 3;
 }
