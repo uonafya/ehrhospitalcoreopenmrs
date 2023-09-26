1b7e8cced9b5b3916e581a7df40a49133330d51d
ningosi <nicotwendelee@gmail.com>
2023-09-26 10:56:03 +0300

    Add descriptive labels to the options for the morgue

--- api/src/main/java/org/openmrs/module/hospitalcore/util/MorgueUtils.java
+++ api/src/main/java/org/openmrs/module/hospitalcore/util/MorgueUtils.java
@@ -1,8 +1,8 @@
 package org.openmrs.module.hospitalcore.util;
 
 public class MorgueUtils {
-    public static final Integer PENDING_ADMISSION = 0;
-    public static final Integer CLEARED_FOR_ADMISSION = 1;
-    public static final Integer ADMITTED = 2;
-    public static final Integer DISCHARGED = 3;
+    public static final Integer BODY_PENDING_ADMISSION = 0;
+    public static final Integer BODY_CLEARED_FOR_ADMISSION = 1;
+    public static final Integer BODY_ADMITTED = 2;
+    public static final Integer BODY_DISCHARGED = 3;
 }
