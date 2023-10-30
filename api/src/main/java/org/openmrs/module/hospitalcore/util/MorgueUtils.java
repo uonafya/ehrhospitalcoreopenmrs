b1b5016100de68176b4839ed6a6625839b60f4d2
ningosi <nicotwendelee@gmail.com>
2023-10-30 15:25:26 +0300

    Interpret the morgue constants

--- api/src/main/java/org/openmrs/module/hospitalcore/util/MorgueUtils.java
+++ api/src/main/java/org/openmrs/module/hospitalcore/util/MorgueUtils.java
@@ -6,4 +6,24 @@ public class MorgueUtils {
     public static final Integer BODY_ADMITTED = 2;
     public static final Integer BODY_DISCHARGED = 3;
     public static final Integer BODY_DECEASED = 99;
+
+    public static String getStringValue(Integer status) {
+        String state = "";
+        if(status.equals(BODY_PENDING_ADMISSION)) {
+            state = "Body pending admission";
+        }
+        else if(status.equals(BODY_CLEARED_FOR_ADMISSION)) {
+            state = "Body cleared for admission";
+        }
+        else if(status.equals(BODY_ADMITTED)) {
+            state = "Body admitted";
+        }
+        else if(status.equals(BODY_DISCHARGED)) {
+            state = "Body discharged";
+        }
+        else if(status.equals(BODY_DECEASED)) {
+            state = "Deceased";
+        }
+        return state;
+    }
 }
