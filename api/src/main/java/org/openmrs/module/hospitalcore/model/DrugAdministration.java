1636ab92576ecc243ba3ff400ceb377de3c1f84c
Nicholas Ingosi <nicotwendelee@gmail.com>
2023-11-08 16:19:52 +0300

    Setters and getters added to the propeties

--- api/src/main/java/org/openmrs/module/hospitalcore/model/DrugAdministration.java
+++ api/src/main/java/org/openmrs/module/hospitalcore/model/DrugAdministration.java
@@ -12,6 +12,87 @@ public class DrugAdministration implements Serializable {
   private Double quantity;
   private Double amount;
   private String status;
+
+  public Integer getDrugAdministrationId() {
+    return drugAdministrationId;
+  }
+
+  public void setDrugAdministrationId(Integer drugAdministrationId) {
+    this.drugAdministrationId = drugAdministrationId;
+  }
+
+  public Integer getDrugOrderId() {
+    return drugOrderId;
+  }
+
+  public void setDrugOrderId(Integer drugOrderId) {
+    this.drugOrderId = drugOrderId;
+  }
+
+  public Double getQuantity() {
+    return quantity;
+  }
+
+  public void setQuantity(Double quantity) {
+    this.quantity = quantity;
+  }
+
+  public Double getAmount() {
+    return amount;
+  }
+
+  public void setAmount(Double amount) {
+    this.amount = amount;
+  }
+
+  public String getStatus() {
+    return status;
+  }
+
+  public void setStatus(String status) {
+    this.status = status;
+  }
+
+  public Date getDrugAdministrationDate() {
+    return drugAdministrationDate;
+  }
+
+  public void setDrugAdministrationDate(Date drugAdministrationDate) {
+    this.drugAdministrationDate = drugAdministrationDate;
+  }
+
+  public Date getCreatedOn() {
+    return createdOn;
+  }
+
+  public void setCreatedOn(Date createdOn) {
+    this.createdOn = createdOn;
+  }
+
+  public User getCreatedBy() {
+    return createdBy;
+  }
+
+  public void setCreatedBy(User createdBy) {
+    this.createdBy = createdBy;
+  }
+
+  public Date getDateModified() {
+    return dateModified;
+  }
+
+  public void setDateModified(Date dateModified) {
+    this.dateModified = dateModified;
+  }
+
+  public User getChangedBy() {
+    return changedBy;
+  }
+
+  public void setChangedBy(User changedBy) {
+    this.changedBy = changedBy;
+  }
+
   private Date drugAdministrationDate;
   private Date createdOn;
   private User createdBy;
